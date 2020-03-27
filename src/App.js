adb5a75f5752f1106aa821de478538d4e3740f74
priyanka_ambawane <priyanka.ambawane@shorelineiot.com>
2020-03-27 14:00:53 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,76 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import InfiniteScroll from "react-infinite-scroll-component";
+import { TableContainer, Paper, TableRow, TableCell, TableHead, TableBody, Table } from "@material-ui/core";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+const style = {
+  border: "1px solid green",
+  margin: 6,
+  padding: 8
+};
+
+class App extends React.Component {
+  state = {
+    items: Array.from({ length: 20 }),
+    hasMore: true
+  };
+
+  fetchMoreData = () => {
+    if (this.state.items.length >= 500) {
+      this.setState({ hasMore: false });
+      return;
+    }
+    // a fake async api call like which sends
+    // 20 more records in .5 secs
+    setTimeout(() => {
+      this.setState({
+        items: this.state.items.concat(Array.from({ length: 20 }))
+      });
+    }, 500);
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Infinite Scroll Demo</h1>
+        <hr />
+        <InfiniteScroll
+          dataLength={this.state.items.length}
+          next={this.fetchMoreData}
+          hasMore={this.state.hasMore}
+          loader={<h4>Loading...</h4>}
+          endMessage={
+            <p style={{ textAlign: "center" }}>
+              <b>Yay! You have seen it all</b>
+            </p>
+          }
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          {/* {this.state.items.map((i, index) => (
+            <div style={style} key={index}>
+              div - #{index}
+            </div>
+          ))} */}
+          <TableContainer style={style} component={Paper}>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell>Div</TableCell>
+                  <TableCell>Index</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {this.state.items.map((i, index) => (
+                  <TableRow key={index}>
+                    <TableCell>Div Tag</TableCell>
+                    <TableCell>{index}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </InfiniteScroll>
+      </div>
+    );
+  }
 }
 
 export default App;
